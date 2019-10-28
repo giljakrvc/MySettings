775b1b517a817f6e49cf87258b496281c64b1176
Joel Gil <46766895+giljakrvc@users.noreply.github.com>
2019-10-28 16:47:01 -0700

    Initial Commit

---
+++ MySettings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySettings
+//
+//  Created by Joel Gil on 10/28/19.
+//  Copyright © 2019 Joel Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
